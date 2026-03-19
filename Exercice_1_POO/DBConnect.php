309a5343dc83a9a31c200dbda1968b4d25b0c856
Gregory BEYLIER <samsung261101@gmail.com>
2026-03-19 11:09:08 +0100

    Ajout sécurisation identifiants BDD avec .env et .gitignore

--- Exercice_1_POO/DBConnect.php
+++ Exercice_1_POO/DBConnect.php
@@ -1,15 +1,20 @@
 <?php
 
 // Fichier : bdd.php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 class DBConnect
 {
     public function getPdo()
     {
         try {
             $pdo = new PDO(
-                'mysql:host=localhost;dbname=gestion_de_contacts;charset=utf8',
-                'root',
-                ''
+                'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8',
+                $_ENV['DB_USER'],
+                $_ENV['DB_PASS']
             );
         } catch (PDOException $e) {
             die('Erreur de connexion : ' . $e->getMessage());
